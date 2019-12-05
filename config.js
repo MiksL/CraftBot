01a979ba8ca47ab729334097b2e6bfd3e17e1e05
Miks <50447221+MiksL@users.noreply.github.com>
2019-12-06 01:24:23 +0200

    Initial login function.

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports =
+{
+    accountName: 'mihailo_zivkovic',
+    password: 'Saabgr5010@',
+    sharedSecret: 'NQQRGbD5CBFOTa8xiSdKaftIHbg='
+}
---
+++ index.js
@@ -0,0 +1,28 @@
+const SteamUser = require('steam-user');
+const SteamTotp = require('steam-totp');
+const TeamFortress2 = require('tf2');
+const config = require('./config');
+
+let client = new SteamUser();
+let tf2 = new TeamFortress2(client);
+
+const logInOptions = 
+{
+    accountName: config.accountName,
+    password: config.password,
+    twoFactorCode: SteamTotp.generateAuthCode(config.sharedSecret)
+};
+
+client.logOn(logInOptions);
+
+client.on('loggedOn', () => {
+    console.log("Logged in!");
+
+    client.setPersona(1);
+    client.gamesPlayed([440]);
+});
+
+//https://github.com/DoctorMcKay/node-tf2
+//https://github.com/DoctorMcKay/node-steam-user
+//https://github.com/Jessecar96/SteamDesktopAuthenticator
+//https://www.youtube.com/watch?v=wZzlcSuf1RI
